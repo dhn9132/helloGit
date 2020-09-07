6d4b8775fc794fe068345cb039131f342cf3e9aa
913219642@qq.com <913219642@qq.com>
2020-09-07 11:14:48 +0800

    branch1

--- HelloGit/src/main/java/com/dhnsoft/git/HelloGit.java
+++ HelloGit/src/main/java/com/dhnsoft/git/HelloGit.java
@@ -1,8 +1,9 @@
-package com.dhnsoft.git;
+package java.com.dhnsoft.git;
 
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1");
+        System.out.println("我是branch1");
     }
 }
